a08a8fb8ebef14e3181e04c10b4d9c0a17a56915
kpcarla01 <kpcarla@gmail.com>
2025-10-31 01:43:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let seleccionadas = [];
 let currentId = null;
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTgubEcPnkc92MYs-sRXj220lvqtlY69I1L_BL5E_c4GxY-FOba0Yc0WBoTvt2U0X-/exec"; // ¡REEMPLAZA CON TU URL!
 
-
 // CARGAR CONFIG
 fetch('./config.json?t=' + Date.now())
   .then(r => r.json())
