40223d0f9de14967269c5c5a0552feb506f7afbf
kpcarla01 <kpcarla@gmail.com>
2025-10-31 01:22:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,6 @@ let seleccionadas = [];
 let currentId = null;
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTgubEcPnkc92MYs-sRXj220lvqtlY69I1L_BL5E_c4GxY-FOba0Yc0WBoTvt2U0X-/exec"; // ¡REEMPLAZA CON TU URL!
 
-const CLIENTE = "lola";
-let fotos = [];
-let seleccionadas = [];
-let currentId = null;
-const SCRIPT_URL = "TU_WEB_APP_URL_AQUI"; // ¡REEMPLAZA!
 
 // CARGAR CONFIG
 fetch('./config.json?t=' + Date.now())
