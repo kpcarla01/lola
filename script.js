53a272a88b28568e0be33df49ef9ab8311784ee1
kpcarla01 <kpcarla@gmail.com>
2025-10-31 01:47:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const CLIENTE = "lola";
 let fotos = [];
 let seleccionadas = [];
 let currentId = null;
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTgubEcPnkc92MYs-sRXj220lvqtlY69I1L_BL5E_c4GxY-FOba0Yc0WBoTvt2U0X-/exec"; // ¡REEMPLAZA CON TU URL!
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyTgubEcPnkc92MYs-sRXj220lvqtlY69I1L_BL5E_c4GxY-FOba0Yc0WBoTvt2U0X-/exec"; // ¡REEMPLAZA CON TU URL! 
 
 // CARGAR CONFIG
 fetch('./config.json?t=' + Date.now())
